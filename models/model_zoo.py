424b59c690093692da1bca0d810284343fd0c595
VongCanhChi <64590894+VongCanhChi@users.noreply.github.com>
2021-08-21 14:16:57 +0700

    Update model_zoo.py

--- models/model_zoo.py
+++ models/model_zoo.py
@@ -260,7 +260,7 @@ MODEL_ZOO = {
         gan_type='stylegan',
         resolution=512,
         #url='https://mycuhk-my.sharepoint.com/:u:/g/personal/1155082926_link_cuhk_edu_hk/EWDWflY6lBpGgX0CGQpd2Z4B5wTEVamTOA9JRYne7zdCvA?e=tOzgYA&download=1',
-        url='https://mega.nz/file/o1R0hBiR#3uFFZbMYu3gP9Kkw2EHeIrqAFm7gnQpOjkKHHkiI3CE',
+        url='https://1drv.ms/u/s!ApqAURLeeYP-kn9H4fpdYapZf5TD?e=g02TNE',
     ),
     'stylegan_animeportrait512': dict(
         gan_type='stylegan',
